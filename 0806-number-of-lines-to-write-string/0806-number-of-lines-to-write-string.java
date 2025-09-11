a83dfa81182f533c696e485758cee6c559274335
christy0112 <amalachristy2501j@gmail.com>
2025-09-11 10:24:05 +0530

    Time: 0 ms (100%), Space: 41.3 MB (98.36%) - LeetHub

---
+++ 0806-number-of-lines-to-write-string/0806-number-of-lines-to-write-string.java
@@ -0,0 +1,19 @@
+class Solution {
+        public int[] numberOfLines(int[] widths, String s) {
+                int numberOfLines = 1;
+                        int lineInPixels = 0;
+
+                                for (char ch : s.toCharArray()) {
+                                            int width = widths[ch - 'a'];
+                                                        
+                                                                    if (lineInPixels + width > 100) {
+                                                                                    numberOfLines++;
+                                                                                                    lineInPixels = width;
+                                                                                                                } else {
+                                                                                                                                lineInPixels += width;
+                                                                                                                                            }
+                                                                                                                                                    }
+
+                                                                                                                                                            return new int[] {numberOfLines, lineInPixels};
+                                                                                                                                                                }
+                                                                                                                                                                }
